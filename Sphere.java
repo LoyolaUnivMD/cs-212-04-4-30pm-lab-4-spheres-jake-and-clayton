8fed5c0fef914823b08ca93c51ce82b471b9ba0b
jakehoogstrate <jakehoogstrate5@gmail.com>
2024-02-14 17:43:54 -0500

    code

--- Sphere.java
+++ Sphere.java
@@ -10,8 +10,9 @@
 *            Reminder: you should never take code from the Internet or another person
 */
 
-
+import java.util.Scanner;
 public class Sphere {
+    Scanner scan = new Scanner(System.in);
     private String name;
     private double xCoordinate;
     private double yCoordinate;
@@ -36,5 +37,26 @@ public class Sphere {
         this.zCoordinate = 0;
         this.radius = 0;
     }
-    public
+    public void calcArea(){
+        double area = (4)*(Math.PI)*(radius*radius);
+        System.out.println(area);
+    }
+    public void calcVolume(){
+        double volume = 4.0/3.0*(Math.PI)*(radius*radius*radius);
+        System.out.println(volume);
+    }
+    public void calcDistance(){
+        System.out.println("X:");
+        double x = scan.nextDouble();
+        System.out.println("Y:");
+        double y = scan.nextDouble();
+        System.out.println("Z:");
+        double z = scan.nextDouble();
+        double distance= Math.sqrt(Math.pow((xCoordinate-x),2)+Math.pow((yCoordinate-y),2)+Math.pow((zCoordinate-z),2));
+        System.out.println(distance);
+    }
+    public void collision(){
+        
+    }
+
 }
