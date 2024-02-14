c028b49bf32cb98a1dd146e1a968141d30aebb66
jakehoogstrate <jakehoogstrate5@gmail.com>
2024-02-14 17:15:58 -0500

    code

--- Main.java
+++ Main.java
@@ -14,28 +14,5 @@ class Main {
         System.out.println("Music of the Spheres");
 
     }
-    public class Spheres {
-        private String name;
-        private double xCoordinate;
-        private double yCoordinate;
-        private double zCoordinate;
-        private double radius;
-    }
 
-    public Spheres(String name, double xCoordinate, double yCoordinate, double zCoordinate, double radius){
-
-        this.name = name;
-        this.xCoordinate= xCoordinate;
-        this.yCoordinate= yCoordinate;
-        this.zCoordinate= zCoordinate;
-        this.radius = radius;
-
-    }
-    public Spheres(){
-        this.name = "Unknown";
-        this.xCoordinate= 0;
-        this.yCoordinate= 0;
-        this.zCoordinate= 0;
-        this.radius = 0;
-    }
 }
